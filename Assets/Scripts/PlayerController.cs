d750feb83b072d6e4d7549b7ba4ee9cf7e32c9ed
Caio Nunes <caionunes3000@gmail.com>
2019-11-17 21:36:32 -0300

    Project setup with 3D FPS and player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+
+  [SerializeField]
+  private float speed = 5f;
+
+  [SerializeField]
+  private float lookSensitivity = 3f;
+
+  private PlayerMotor motor;
+
+  void Start()
+  {
+    motor = GetComponent<PlayerMotor>();
+  }
+
+  void Update()
+  {
+    float xAxis = Input.GetAxisRaw("Horizontal");
+    float zAxis = Input.GetAxisRaw("Vertical");
+
+    Vector3 moveHorizontal = transform.right * xAxis;
+    Vector3 moveVertical = transform.forward * zAxis;
+
+    Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+    motor.Move(velocity);
+
+    float yRot = Input.GetAxisRaw("Mouse X");
+
+    Vector3 rotation = new Vector3(0f, yRot, 0f) * lookSensitivity;
+
+    motor.Rotate(rotation);
+
+    float xRot = Input.GetAxisRaw("Mouse Y");
+
+    Vector3 cameraRotation = new Vector3(xRot, 0f, 0f) * lookSensitivity;
+
+    motor.RotateCamera(cameraRotation);
+
+  }
+
+}
