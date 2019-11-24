a0ea2c2566fe9ff4d2d2c02c4f85781b64441fc7
Ulisses Lopes <ulissesdvl@yahoo.com.br>
2019-11-23 23:05:14 -0300

    Adding shooting mechanics

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -5,6 +5,8 @@ public class PlayerSetup : NetworkBehaviour
 {
   [SerializeField]
   Behaviour[] componentsToDisable;
+  [SerializeField]
+	string remoteLayerName = "RemotePlayer";
 
   Camera sceneCamera;
 
@@ -12,10 +14,8 @@ public class PlayerSetup : NetworkBehaviour
   {
     if (!isLocalPlayer)
     {
-      for (int i = 0; i < componentsToDisable.Length;  i++)
-      {
-        componentsToDisable[i].enabled = false;
-      }
+      DisableComponents();
+			AssignRemoteLayer();
     } else 
     {
       sceneCamera = Camera.main;
@@ -25,7 +25,28 @@ public class PlayerSetup : NetworkBehaviour
       }
       
     }
+    
+    RegisterPlayer ();
   }
+
+  void RegisterPlayer ()
+	{
+		string _ID = "Player " + GetComponent<NetworkIdentity>().netId;
+		transform.name = _ID;
+	}
+
+	void AssignRemoteLayer ()
+	{
+		gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+	}
+
+	void DisableComponents ()
+	{
+		for (int i = 0; i < componentsToDisable.Length; i++)
+		{
+			componentsToDisable[i].enabled = false;
+		}
+	}
 
   void OnDisable()
   {
